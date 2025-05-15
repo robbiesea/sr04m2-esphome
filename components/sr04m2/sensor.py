30ac2649c22dc2810609f5cd30d5c8689015d61b
RobbieSea <48457961+robbiesea@users.noreply.github.com>
2025-05-16 09:20:51 +1000

    Update sensor.py

--- components/sr04m2/sensor.py
+++ components/sr04m2/sensor.py
@@ -1,25 +1,29 @@
-from esphome import automation
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, sensor
-from esphome.const import UNIT_CENTIMETER, DEVICE_CLASS_DISTANCE
+from esphome.const import (
+    UNIT_CENTIMETER,
+    DEVICE_CLASS_DISTANCE,
+    STATE_CLASS_MEASUREMENT,
+)
 
 DEPENDENCIES = ["uart"]
 
-sr04m2_ns = cg.esphome_ns.namespace('sr04m2')
-SR04M2Sensor = sr04m2_ns.class_('SR04M2Sensor', sensor.Sensor, cg.Component, uart.UARTDevice)
+sr04m2_ns = cg.esphome_ns.namespace("sr04m2")
+SR04M2Sensor = sr04m2_ns.class_("SR04M2Sensor", sensor.Sensor, cg.PollingComponent, uart.UARTDevice)
 
 CONFIG_SCHEMA = sensor.sensor_schema(
     unit_of_measurement=UNIT_CENTIMETER,
-    icon="mdi:waves",
     accuracy_decimals=1,
-    device_class=DEVICE_CLASS_DISTANCE
+    device_class=DEVICE_CLASS_DISTANCE,
+    state_class=STATE_CLASS_MEASUREMENT,
 ).extend({
     cv.GenerateID(): cv.declare_id(SR04M2Sensor),
-}).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+    cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
+}).extend(cv.polling_component_schema("1s"))
 
-def to_code(config):
-    var = cg.new_Pvariable(config[cv.ID])
-    yield cg.register_component(var, config)
-    yield uart.register_uart_device(var, config)
-    yield sensor.register_sensor(var, config)
+async def to_code(config):
+    var = cg.new_Pvariable(config[cv.GenerateID])
+    await cg.register_component(var, config)
+    await sensor.register_sensor(var, config)
+    await uart.register_uart_device(var, config)
